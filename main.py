582d53ab63a939bb328ed79e9e41ac0193ff1286
Sebastian Cifredo <sebastiancifredo@gmail.com>
2024-09-18 13:00:55 -0400

    transcription logic using SpeechRecognizer's Google Speech API.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from src.convert import convert_m4a_to_wav
+from src.transcribe import transcribe_audio
 
 def main(input_m4a):
-    wav_file = "temp_audio.wav"
-    convert_m4a_to_wav(input_m4a,wav_file)
+    #wav_file = "temp_audio.wav"
+    #convert_m4a_to_wav(input_m4a,wav_file)
+
+    return transcribe_audio("temp_audio.wav")
+
 
 
 if __name__ == "__main__":
