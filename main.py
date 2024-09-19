fbba4026b241d63d53a5c3abb37e2e877116f9fc
Sebastian Cifredo <sebastiancifredo@gmail.com>
2024-09-19 16:48:22 -0400

    Speech Diarization using AssemblyAI

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ import os
 from src.convert import convert_m4a_to_wav
 from src.transcribe import transcribe_audio
 from src.fileutils import save_transcription
+from src.assemblyTranscribe import assemblyTranscribe, assemblyDiaritization
 
 def main(input_m4a,output_txt):
     #wav_file = "temp_audio.wav"
     #convert_m4a_to_wav(input_m4a,wav_file)
 
-    transcription = transcribe_audio("temp_audio.wav")
+    transcription = assemblyDiaritization("temp_audio.wav")
 
-    save_transcription(transcription, output_txt)
+    #save_transcription(transcription, output_txt)
 
 
 
 if __name__ == "__main__":
-    input_m4a = "videoplayback.m4a"
+    input_m4a = "ME PERDI EN EL MAR CON MI VECINO!.m4a"
     output_txt = "transcription.txt"
     main (input_m4a,output_txt)
---
+++ src/assemblyTranscribe.py
@@ -0,0 +1,40 @@
+# Start by making sure the `assemblyai` package is installed.
+# If not, you can install it by running the following command:
+# pip install -U assemblyai
+#
+# Note: Some macOS users may need to use `pip3` instead of `pip`.
+
+import assemblyai as aai
+
+# Replace with your API key
+aai.settings.api_key = "a062defa71084271b5dbe8d7e639a106"
+
+# URL of the file to transcribe
+#FILE_URL = "https://github.com/AssemblyAI-Community/audio-examples/raw/main/20230607_me_canadian_wildfires.mp3"
+
+# You can also transcribe a local file by passing in a file path
+# FILE_URL = './path/to/file.mp3'
+
+
+def assemblyTranscribe(file_path):
+
+    transcriber = aai.Transcriber()
+    transcript = transcriber.transcribe(file_path)
+
+    if transcript.status == aai.TranscriptStatus.error:
+        return(transcript.error)
+    else:
+        return(transcript.text)
+
+
+def assemblyDiaritization(file_path):
+    config = aai.TranscriptionConfig(speaker_labels=True, language_code="es")
+
+    transcriber = aai.Transcriber()
+    transcript = transcriber.transcribe(
+    file_path,
+    config=config
+    )
+
+    for utterance in transcript.utterances:
+        print(f"Speaker {utterance.speaker}: {utterance.text}")
